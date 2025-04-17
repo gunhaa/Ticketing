736a7f48ea10cc16269d7dbcc2646c3a2e994e31
gunha <wh8299@gmail.com>
2025-04-17 13:55:38 +0900

    feat : index, middleware 설정 추가

--- producer/src/index.js
+++ producer/src/index.js
@@ -4,11 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config();
+console.log(path_1.default);
 const app = (0, express_1.default)();
-const port = 5000;
-app.get('/', (req, res) => {
-    res.send('Typescript + Node.js + Express Server');
+const port = process.env.PORT;
+app.use((req, res, next) => {
+    console.log('첫 번째 미들웨어');
+    next();
 });
+app.use((req, res, next) => {
+    console.log('현재 시간:', new Date().toISOString());
+    next();
+});
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.listen(port, () => {
+    console.log("listen 실행..");
     console.log(`[server]: Server is running at <https://localhost>:${port}`);
 });
--- producer/src/index.ts
+++ producer/src/index.ts
@@ -1,12 +1,27 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+console.log(path);
 
 const app: Express = express();
-const port = 5000;
+const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Typescript + Node.js + Express Server');
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('첫 번째 미들웨어');
+  next();
 });
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('현재 시간:', new Date().toISOString());
+  next(); 
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.listen(port, () => {
+  console.log("listen 실행..");
   console.log(`[server]: Server is running at <https://localhost>:${port}`);
 });
