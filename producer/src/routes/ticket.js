e7268e328f30aabfd159b5e3e5eacf6847cbf5fb
gunha <wh8299@gmail.com>
2025-04-18 14:17:08 +0900

    feat : ticket uuid인증/redis을 통한 처리로직 추가, /ticket/wating 라우터에서 userUuid cookie를 이용한 로직 개발 예정

--- producer/src/routes/ticket.js
+++ producer/src/routes/ticket.js
@@ -5,8 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const path_1 = __importDefault(require("path"));
+const redis_1 = __importDefault(require("../config/redis"));
+const uuid_1 = require("uuid");
 const router = (0, express_1.Router)();
-router.get('/', (req, res, next) => {
-    res.sendFile(path_1.default.join(__dirname, '..', 'public', 'ticket.html'));
+const maxRequestLimit = 5;
+const queueKey = 'ticketQueue';
+router.get('/', async (req, res, next) => {
+    try {
+        const userUuid = (0, uuid_1.v4)();
+        console.log(`this uuid ${userUuid}`);
+        await redis_1.default.rPush(queueKey, userUuid);
+        res.cookie('userUuid', userUuid, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60,
+        });
+        const listLength = await redis_1.default.lLen(queueKey);
+        if (listLength > maxRequestLimit) {
+            return res.redirect(`/ticket/waiting`);
+        }
+        res.sendFile(path_1.default.join(__dirname, '..', 'public', 'ticket.html'));
+    }
+    catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 exports.default = router;
--- producer/src/routes/ticket.ts
+++ producer/src/routes/ticket.ts
@@ -1,15 +1,38 @@
 import {Router, Request, Response, NextFunction} from 'express';
 import path from 'path';
 import redisClient from '../config/redis'
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = Router();
 
-router.get('/', (req:Request, res:Response, next:NextFunction) => {
-    res.sendFile(path.join(__dirname, '..' ,'public', 'ticket.html'));
-});
+const maxRequestLimit = 5; 
+const queueKey = 'ticketQueue';
 
-router.get('/v1/redis', (req:Request, res:Response, next:NextFunction)  => {
+router.get('/', async (req:Request, res:Response, next:NextFunction) => {
     
+    try {
+        const userUuid = uuidv4();
+        console.log(`this uuid ${userUuid}`);
+    
+        await redisClient.rPush(queueKey, userUuid);
+
+        res.cookie('userUuid', userUuid, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60, 
+        });
+    
+        const listLength = await redisClient.lLen(queueKey);
+        if (listLength > maxRequestLimit) {
+            return res.redirect(`/ticket/waiting`);
+        }
+    
+        res.sendFile(path.join(__dirname, '..', 'public', 'ticket.html'));
+    
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 export default router;
