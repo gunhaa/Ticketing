c71f2910ca6da60a71050ee82f34d97c00beb056
gunha <wh8299@gmail.com>
2025-06-18 12:55:12 +0900

    feat: spring 프로젝트 추가, 의존성 추가

---
+++ ticketing/src/main/java/com/ticket/gunha/GunhaApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.gunha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunhaApplication.class, args);
+	}
+
+}
